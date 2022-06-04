85ece3ab73ca62d58f3345726cbb3cd4b8aabc9f
Ebrahim15 <105316494+Ebrahim15@users.noreply.github.com>
2022-06-04 13:48:58 +0200

    Update Store.js

--- Ecommerce/frontend/src/Redux/Store.js
+++ Ecommerce/frontend/src/Redux/Store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import cartReducer from './Features/Cart/CartSlice'
 import adminReducer from './Features/Admin/AdminSlice'
-export const store = configureStore({
-    reducer: { cart: cartReducer, admin: adminReducer },
-})
+// export const store = configureStore({
+//     reducer: { cart: cartReducer, admin: adminReducer },
+// })
